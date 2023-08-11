d20f5b5581891c65e175c39b3ce43082c0464330
Mrityunjay Prasad Singh <Mrityunjay.singh119@gmail.com>
2023-08-11 19:26:02 +0530

    Chat Send and Recived Module Done(2) final

--- app/src/main/java/com/example/chatapp/activities/ChatActivity.java
+++ app/src/main/java/com/example/chatapp/activities/ChatActivity.java
@@ -5,12 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityChatBinding;
+import com.example.chatapp.models.User;
+import com.example.chatapp.utilities.Constants;
 
 public class ChatActivity extends AppCompatActivity {
+
+    private ActivityChatBinding binding;
+    private User reciverUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat);
+        binding = ActivityChatBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+        loadReciverDetails();
+    }
+
+    private void loadReciverDetails() {
+        reciverUser = (User) getIntent().getSerializableExtra(Constants.KEY_USER);
+        binding.textname.setText(reciverUser.name);
+    }
+
+    private void setListeners() {
+        binding.imageBack.setOnClickListener(v -> onBackPressed());
     }
 }
--- app/src/main/java/com/example/chatapp/activities/UsersActivity.java
+++ app/src/main/java/com/example/chatapp/activities/UsersActivity.java
@@ -1,26 +1,25 @@
 package com.example.chatapp.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chatapp.adapters.UsersAdapter;
 import com.example.chatapp.databinding.ActivityUsersBinding;
+import com.example.chatapp.listeners.UserListener;
+import com.example.chatapp.listeners.UserListener;
 import com.example.chatapp.models.User;
 import com.example.chatapp.utilities.Constants;
 import com.example.chatapp.utilities.PrefrenceManager;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Queue;
 
-public class UsersActivity extends AppCompatActivity {
+public class UsersActivity extends AppCompatActivity implements UserListener {
 
     private ActivityUsersBinding binding;
     private PrefrenceManager prefrenceManager;
@@ -60,7 +59,7 @@ public class UsersActivity extends AppCompatActivity {
                             users.add(user);
                         }
                         if (users.size() > 0) {
-                            UsersAdapter usersAdapter = new UsersAdapter(users);
+                            UsersAdapter usersAdapter = new UsersAdapter(users, this);
                             binding.usersRecyclerView.setAdapter(usersAdapter);
                             binding.usersRecyclerView.setVisibility(View.VISIBLE);
                         }else {
@@ -83,5 +82,13 @@ public class UsersActivity extends AppCompatActivity {
     private void showErrorMessage() {
         binding.textErrorMessage.setText(String.format("%s", "No user found"));
         binding.textErrorMessage.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void onUserClicked(User user) {
+        Intent intent = new Intent(getApplicationContext(), ChatActivity.class);
+        intent.putExtra(Constants.KEY_USER, user);
+        startActivity(intent);
+        finish();
     }
 }
